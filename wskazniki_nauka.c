4895f637fab39adc8c7fc07600fa88c84dce895e
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-03-16 14:38:38 +0100

    Update wskazniki_nauka.c

--- wskazniki_nauka.c
+++ wskazniki_nauka.c
@@ -9,7 +9,7 @@ void dodaj_jeden(int *a, int *b) {
 
 int main()
 {
-    // Wskaüniki tutorial - fast
+    // Wska≈∫niki tutorial - fast
     // int zmienna = 5;
     // int * wskaznikZmiennej = &zmienna;
     // printf("Wartosc zmiennej: %d\n", *wskaznikZmiennej);
@@ -19,16 +19,45 @@ int main()
     // printf("Miejsce pamieci : %p\n", wskaznikZmiennej);
     // *wskaznikZmiennej = 8;
     // printf("Wartosc zmiennej: %d\n", zmienna);
-    // & <= wskaünik do pamiÍci
-    // * <= wskaünik do wartoúci
-    // int * a; - b≥πd (2 od razu na egzaminie, musi siÍ do czegoúÊ odwo≥ywaÊ)
+    // & <= wska≈∫nik do pamiƒôci
+    // * <= wska≈∫nik do warto≈õci
+    // int * a; - b≈ÇƒÖd (2 od razu na egzaminie, musi siƒô do czego≈õƒá odwo≈Çywaƒá)
     int a = 5, b = 7;
     printf("%d %d\n", a, b);
     dodaj_jeden(&a, &b); // <- dodajesz wskaznik do pamieci
     printf("%d %d\n", a, b);
+   
+   // static w c
+//    void foo()
+// {
+//     int a = 10;
+//     static int sa = 10;
 
-    // malloc(sizeof(int)*10) <= s≥uøy do deklarowania miejsca w pamiÍci (przyk≥ad to rozmiar dla 1 inta) (* 10 = alokuje miejsce dla 10 intow pod rzπd, tworzy tablice dynamiczna)
-    // Po wykonaniu waøne by uøyÊ funkcji free(wsk) - by zwolniÊ pamiÍÊ
+//     a += 5;
+//     sa += 5;
+
+//     printf("a = %d, sa = %d\n", a, sa);
+// }
+//    int i;
+
+//     for (i = 0; i < 10; ++i)
+//         foo();
+   
+// a = 15, sa = 15
+// a = 15, sa = 20
+// a = 15, sa = 25
+// a = 15, sa = 30
+// a = 15, sa = 35
+// a = 15, sa = 40
+// a = 15, sa = 45
+// a = 15, sa = 50
+// a = 15, sa = 55
+// a = 15, sa = 60
+
+   
+
+    // malloc(sizeof(int)*10) <= s≈Çu≈ºy do deklarowania miejsca w pamiƒôci (przyk≈Çad to rozmiar dla 1 inta) (* 10 = alokuje miejsce dla 10 intow pod rzƒÖd, tworzy tablice dynamiczna)
+    // Po wykonaniu wa≈ºne by u≈ºyƒá funkcji free(wsk) - by zwolniƒá pamiƒôƒá
     int * wsk = malloc(sizeof(int) * 10);
     printf("%p\n", wsk);
     wsk[0] = 1;
